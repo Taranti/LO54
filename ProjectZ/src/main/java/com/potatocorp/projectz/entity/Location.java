49bc7806d828f5ab327b031c74faa20fdec102f1
Thomas Gubien <thomas.gubien@outlook.fr>
2019-11-16 11:05:04 +0100

    Add locations and location dao

--- ProjectZ/src/main/java/com/potatocorp/projectz/entity/Location.java
+++ ProjectZ/src/main/java/com/potatocorp/projectz/entity/Location.java
@@ -5,10 +5,72 @@
  */
 package com.potatocorp.projectz.entity;
 
+import java.util.Objects;
+
 /**
  *
  * @author thoma
  */
 public class Location {
+    private int id;
+    private String city;
+
+    public Location() {
+    }
+
+    public Location(int id, String city) {
+        this.id = id;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + this.id;
+        hash = 83 * hash + Objects.hashCode(this.city);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.city, other.city)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "id=" + id + ", city=" + city + '}';
+    }
     
 }
--- ProjectZ/src/main/java/com/potatocorp/projectz/repository/MYSQLLocationDAO.java
+++ ProjectZ/src/main/java/com/potatocorp/projectz/repository/MYSQLLocationDAO.java
@@ -5,10 +5,62 @@
  */
 package com.potatocorp.projectz.repository;
 
+import com.potatocorp.projectz.entity.Location;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /**
  *
  * @author thoma
  */
 public class MYSQLLocationDAO {
+    public void saveNewLocation(Location l) {
+        Connection con = null;
+        try {
+            Class.forName("org.hibernate.dialect.MySQLDialect");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/projectz", "root", "root");
+            PreparedStatement ps = con.prepareStatement("INSERT INTO LOCATION(CITY) VALUES(?)");
+            ps.setString(1, l.getCity());
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     
+    public ArrayList<Location> getLocations(){
+        ArrayList<Location> locations = new ArrayList<Location>();
+        Connection con = null;
+        String sql = "SELECT * FROM CLIENT";
+        try {
+            Class.forName("org.hibernate.dialect.MySQLDialect");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/projectz", "root", "root");
+            Statement s = con.createStatement();
+            ResultSet rs = s.executeQuery(sql);
+            while(rs.next()){
+                Location l = new Location(rs.getInt("ID"),rs.getString("CITY"));
+                locations.add(l);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+
+            }
+        }
+        return locations;   
+    }
 }
